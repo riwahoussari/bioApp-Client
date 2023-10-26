59cb5930e4af05f34b00b94f3d91c160a9f9e66a
riwahoussari <houssaririwa63@gmail.com>
2023-10-26 09:36:00 -0700

    fix games header

--- src/games/RedLightGreenLight.jsx
+++ src/games/RedLightGreenLight.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 import '../stylesheets/redLightGreenLight.css'
 import { useUser } from '../hooks/useUser';
 
